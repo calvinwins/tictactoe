e22f63891fbc0b70b5865a7d9bd671c4b1a77f20
calvinwins <weaver95@byui.edu>
2022-04-01 15:31:43 -0500

    as

--- tictac.py
+++ tictac.py
@@ -2,6 +2,7 @@
 # Tic tac toe
 # using git
 def main():
+    print('Tic Tac Toe')
     board = new_board()
     player = next_player("")
     while not (winner(board) or draw(board)):
@@ -9,7 +10,12 @@ def main():
         make_move(player, board)
         player = next_player(player)
     display_board(board)
-    print("Good game. Thanks for playing!") 
+    if draw(board):
+        print('Tie Game!')
+    elif winner(board, 'o'):
+        print('The Machine Won')
+    else:
+        print("You Won! Thanks for playing!") 
 
 
 def new_board():
@@ -32,19 +38,21 @@ def display_board(board):
     -----
     {board[6]}|{board[7]}|{board[8]}''')
     print()
-def winner(board):
-    return (board[0] == board[1] == board[2] or
-            board[3] == board[4] == board[5] or
-            board[6] == board[7] == board[8] or
-            board[0] == board[3] == board[6] or
-            board[1] == board[4] == board[7] or
-            board[2] == board[5] == board[8] or
-            board[0] == board[4] == board[8] or
-            board[2] == board[4] == board[6])
+def winner(board, le):
+    return (board[0] == le and board[1] == le and board[2] == le or
+            board[3] == le and board[4] == le and board[5] == le or
+            board[6] == le and board[7] == le and board[8] == le or
+            board[0] == le and board[3] == le and board[6] == le or
+            board[1] == le and board[4] == le and board[7] == le or
+            board[2] == le and board[5] == le and board[8] == le or
+            board[0] == le and board[4] == le and board[8] == le or
+            board[2] == le and board[4] == le and board[6] == le)
 
 def make_move(player, board):
     square = int(input(f"{player}'s turn to choose a square (1-9): "))
     board[square - 1] = player
+
+
 
 def next_player(current):
     if current == "" or current == "o":
